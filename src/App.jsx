f6531bee3aadd46be0a733690d5fb3d8bb088171
ali <ali.hassan.salman.2000@gmail.com>
2025-10-22 19:20:58 +0300

    style btn 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 
-import Button1 from './components/example-1-normal-css/Button';
+import Button1 from './components/example-1-normal-css/Button.jsx';
+import Button2 from './components/example-2-inline-css/Button.jsx';
 
 function App() {
 
@@ -11,7 +12,15 @@ function App() {
       This means they might conflict if you use the same class or id names in different files. */}
       <div className="App"> <Button1 buttonText="Example 1" /> </div>
 
-      
+      {/*Inline styling in React is particularly useful for dynamic styling. Since styles are 
+      applied as JavaScript objects, you can easily modify styles based on the component’s state 
+      or props. This allows for more flexible and interactive styling that changes in response 
+      to user actions or other conditions within the application. As the application grows, 
+      managing styles inline can become cumbersome and less maintainable. This approach can 
+      lead to code duplication and difficulty in tracking style-related bugs. */}
+      <div className="App"> <Button2 buttonText="Example 2" /> </div>
+
+
     </>
     
   )
