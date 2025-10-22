7a625e06a18678c9b4dd8239557e2ab73ad66c94
ali <ali.hassan.salman.2000@gmail.com>
2025-10-22 19:05:02 +0300

    style btn 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
 
+import Button1 from './components/example-1-normal-css/Button';
+
 function App() {
 
   return (
-    <div className="App">
-      {
-        // Add all example components:
-        <Button1 buttonText="Example 1" />
-        //...
-      }
-    </div>
+    <>
+      {/* Using external CSS files to style React components is straightforward and familiar. 
+      However, it’s important to remember that the styles defined in these files are global. 
+      This means they might conflict if you use the same class or id names in different files. */}
+      <div className="App"> <Button1 buttonText="Example 1" /> </div>
+
+      
+    </>
+    
   )
 }
 
