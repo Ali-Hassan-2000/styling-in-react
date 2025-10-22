1a5728a2ece9357365839ab3d0fd3243619fa5a7
ali <ali.hassan.salman.2000@gmail.com>
2025-10-22 18:57:02 +0300

    add styling comp.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import './App.css'
 function App() {
 
   return (
-    <>
-
-    </>
+    <div className="App">
+      {
+        // Add all example components:
+        <Button1 buttonText="Example 1" />
+        //...
+      }
+    </div>
   )
 }
 
